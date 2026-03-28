35cb56d459f2b8e3ac54aacaed5455cca267471a
sanherR <santihernandezrosa@gmail.com>
2026-03-28 18:03:04 +0100

    preuba

--- backen/obj/Debug/net9.0/backen.AssemblyInfo.cs
+++ backen/obj/Debug/net9.0/backen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf7922268248adbfcbd8ed24e2d199369c01355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba905aaf635850a400909a8fd61adbb67a1db565")]
 [assembly: System.Reflection.AssemblyProductAttribute("backen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
